c866adc016b8f597d80dba013677384a79e040b6
Dannystu12 <mgdog1@googlemail.com>
2018-10-07 16:43:29 +0100

    added camera scroll

--- Assets/Scripts/IsoCamera.cs
+++ Assets/Scripts/IsoCamera.cs
@@ -4,17 +4,38 @@ using UnityEngine;
 
 public class IsoCamera : MonoBehaviour {
 
+    [SerializeField] float zoomSpeed = 4f;
+    [SerializeField] float minZoom = 5f;
+    [SerializeField] float maxZoom = 40f;
+    [SerializeField] float currentZoom = 15f;
+
+
     Player player;
     Vector3 offset;
+    Camera camera;
 
 	// Use this for initialization
 	void Start () {
         player = FindObjectOfType<Player>();
         offset = player.transform.position - gameObject.transform.position;
-	}
+        camera = GetComponent<Camera>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        gameObject.transform.position = player.transform.position - offset;
+        UpdateZoom();
+        TrackPlayer();
 	}
+
+    private void TrackPlayer()
+    {
+        gameObject.transform.position = player.transform.position - offset;
+    }
+
+    private void UpdateZoom()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+        camera.fieldOfView = currentZoom;
+    }
 }
