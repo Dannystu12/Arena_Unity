b6cc3be7d57f1e9d997a57fe23e627af57b8489e
Dannystu12 <mgdog1@googlemail.com>
2018-10-09 15:22:15 +0100

    Addedgoblin enemy data model to game

---
+++ Assets/Scripts/Models/Enemies/Enemy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Character {
+    public Enemy(int hp, int armourClass, int damageRoll)
+        : base(hp, armourClass, damageRoll)
+    {
+    }
+}
---
+++ Assets/Scripts/Models/Enemies/Goblin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : Enemy {
+
+    [SerializeField] const int healthRoll = 8;
+    [SerializeField] const int armouClass = 9;
+    [SerializeField] const int damageRoll = 10;
+
+    public Goblin()
+        : base(Dice.Roll(healthRoll) + 4, armouClass, damageRoll)
+    {
+
+    }
+}
