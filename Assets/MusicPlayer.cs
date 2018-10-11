d56c9733c97a6b2afa5cfaa2eb10667a85097461
Dannystu12 <mgdog1@googlemail.com>
2018-10-11 14:35:50 +0100

    Changed background music to singleton that plays into game over

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		if(FindObjectsOfType<MusicPlayer>().Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+	}
+
+    private void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
