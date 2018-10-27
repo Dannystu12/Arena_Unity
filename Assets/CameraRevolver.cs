354e212855f2bc607755ebd760a2aa1f79c7540a
Dannystu12 <mgdog1@googlemail.com>
2018-10-27 18:21:48 +0100

    Added rotating arena in menu and game over

---
+++ Assets/CameraRevolver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRevolver : MonoBehaviour {
+
+    [SerializeField] float rotationSpeed = 0.25f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, rotationSpeed, 0);
+    }
+}
